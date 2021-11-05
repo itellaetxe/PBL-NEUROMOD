087dd10559b5460f8d5491e601eb206fa8417f32
iisasa <92914512+iisasa@users.noreply.github.com>
2021-11-05 12:53:55 +0100

    Add files via upload

---
+++ Data_selection_processing.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov  5 12:50:24 2021
+
+@author: IMANOL
+"""
+
+import pandas as pd
+
+datasetV1 = pd.read_csv("./Trial_1_11_04_2021.csv")
+datasetV1.columns
+
+rowsToDelete = []
+for sample in range(datasetV1.shape[0]):
+    mod = datasetV1['Modality']
+    if mod[sample] != 'DTI':
+        rowsToDelete.append(sample)
+
+datasetDTI = datasetV1.drop(rowsToDelete, axis=0, inplace=False)
+datasetDTI.drop('Modality', axis=1, inplace=True)
+datasetDTI.reset_index(drop=True)
