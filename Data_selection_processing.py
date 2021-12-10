949994aa4b48a59f55c18f21e75e2e8209ff1fb4
itellaetxe <inigo.tellaetxe@alumni.mondragon.edu>
2021-12-10 08:58:21 +0100

    CSV addition

--- Data_selection_processing.py
+++ Data_selection_processing.py
@@ -8,25 +8,32 @@ Created on Fri Nov  5 12:50:24 2021
 import pandas as pd
 import numpy as np
 
-dataset = pd.read_csv("./ADNI_Screening_12_09_2021_dataset")
+dataset = pd.read_csv("./idaSearch_12_09_2021.csv")
 dataset.columns
 
-# for sample, idx in zip(dataset['Research Group'], range(0, len(dataset['Research Group']))):
-#     if sample == 'EMCI' or sample == 'LMCI':
-#         dataset['Research Group'][idx] = 'MCI'
-#
-# MCIcount=0
-# SMCcount=0
-# CNcount=0
-# ADcount=0
-# for sample in dataset['Research Group']:
-#     if sample == 'MCI':
-#         MCIcount += 1
-#     if sample == 'SMC':
-#         SMCcount += 1
-#     if sample == 'CN':
-#         CNcount += 1
-#     if sample == 'AD':
-#         ADcount += 1
-        
-numOfPatients = np.unique(dataset['Subject ID'])
+for sample, idx in zip(dataset['Research Group'], range(0, len(dataset['Research Group']))):
+    if sample == 'EMCI' or sample == 'LMCI':
+        dataset['Research Group'][idx] = 'MCI'
+
+MCIcount=0
+SMCcount=0
+CNcount=0
+ADcount=0
+for sample in dataset['Research Group']:
+    if sample == 'MCI':
+        MCIcount += 1
+    if sample == 'SMC':
+        SMCcount += 1
+    if sample == 'CN':
+        CNcount += 1
+    if sample == 'AD':
+        ADcount += 1
+
+descriptionTypes = np.unique(dataset['Description'])
+print(descriptionTypes)
+
+patientNum = np.unique(dataset['Subject ID'])
+print(len(patientNum))
+
+
+
