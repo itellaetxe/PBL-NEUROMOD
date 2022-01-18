f04f4b4093894f2faf3341bc9463627ebea5aa6b
iisasa <imanol.isasa@alumni.mondragon.edu>
2022-01-18 10:55:34 +0100

    requirements.txt updated

--- MultiLayer_Perceptron_model.py
+++ MultiLayer_Perceptron_model.py
@@ -1,4 +1,3 @@
-import numpy as np
 import pandas as pd
 from tensorflow.keras import Model
 from tensorflow.keras.layers import Dense, Dropout, Activation
@@ -8,7 +7,8 @@ from tensorflow.keras.callbacks import ModelCheckpoint
 from sklearn.model_selection import train_test_split
 from imblearn.combine import SMOTETomek
 
-def mlp_model(parameters):
+
+def mlp_model(parameters, x_train, y_train, x_test, y_test):
     """
     param parameters:
     return: model, history
@@ -18,24 +18,21 @@ def mlp_model(parameters):
     if parameters['num_of_layers'] != (len(parameters['num_of_neurons']) or len(parameters['dropout_vals'])):
         raise Exception("Parameters are not consistent.")
 
-    # Load data
-    X_train = pd.read(...)
-    Y_train = pd.read(...)
-    X_test = pd.read(...)
-    Y_test = pd.read(...)
-    xtr, xval, ytr, yval = train_test_split(X_train, Y_train, test_size=0.2, random_state=42, stratify=Y_train)
+    xtr, xval, ytr, yval = train_test_split(x_train, y_train, test_size=0.2, random_state=42, stratify=y_train)
 
     # If SMOTETomek is wanted to be applied
-    if parameters['imbalanced'] == True:
+    if parameters['imbalanced']:
         smtom = SMOTETomek(random_state=42)
         xtr, ytr = smtom.fit_resample(xtr, ytr)
+
+    input_variables = xtr.shape[1]
 
     neurons = parameters['num_of_neurons']
     dropouts = parameters['dropout_vals']
 
     # Build model architecture
     model = Model.Sequential()
-    model.add(Dense(neurons[0], input_shape=(inputVariables,), activation='relu'))
+    model.add(Dense(neurons[0], input_shape=(input_variables,), activation='relu'))
 
     for layer in range(0, len(parameters['num_of_layers']), 1):
         model.add(Dropout(dropouts[layer]))
@@ -62,13 +59,27 @@ def mlp_model(parameters):
                         callbacks=[mcp])
     print("------------------- TRAINING FINISHED -------------------")
 
-parameters = {'num_of_neurons': [8, 8, 8], # [layer0, layer1, layer2, ...]
+    return history, model
+
+
+x_train = pd.read_csv('C:\\', encoding='latin1')
+y_train = pd.read_csv('C:\\', encoding='latin1')
+x_test = pd.read_csv('C:\\', encoding='latin1')
+y_test = pd.read_csv('C:\\', encoding='latin1')
+
+parameters = {'num_of_neurons': [32, 32],  # [layer0, layer1, layer2, ...]
               'dropout_vals': [0.5, 0.5, 0.5],
-              'batch_size': 32, # 32, 64, 128, ...
-              'num_of_layers': 4, # total = num_of_layers + 1
+              'batch_size': 64,  # 32, 64, 128, ...
+              'num_of_layers': 2,  # total = num_of_layers + 1
               'lr': 0.0001,
               # 'sgd_momentum': 0.4371162594318422, # Just if SGD optimizer is selected
               'epoch': 5000,
               'imbalanced': False}
 
-mlp_model(parameters)
+history, model = mlp_model(parameters, x_train, y_train, x_test, y_test)
+output_dir = './output_MLP/'
+model.save(output_dir + 'output_MLP_model.h5')
+
+scores = pd.DataFrame()
+loss, accuracy, auc, pre, rec = model.evaluate(x_test, y_test, verbose=1)
+
