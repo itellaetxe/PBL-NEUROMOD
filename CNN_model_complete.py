b634f1df55b0eb91066d138a7acbbb62cdf7080a
iisasa <imanol.isasa@alumni.mondragon.edu>
2022-05-30 13:01:29 +0200

    CNN model evaluation started

--- CNN_model_complete.py
+++ CNN_model_complete.py
@@ -1,16 +1,44 @@
-import pandas as pd
-from tensorflow.keras import Sequential
-from tensorflow.keras.layers import Dense, Dropout, Activation, Conv2D, MaxPooling2D, Flatten
-from tensorflow.keras.optimizers import SGD, Adam, RMSprop
-from tensorflow.keras.metrics import Precision, Recall
-from tensorflow.keras.callbacks import ModelCheckpoint
+import os
+import re
+import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from imblearn.combine import SMOTETomek
 from scipy.io import loadmat
-import numpy as np
+
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Dense, Activation, Conv2D, MaxPooling2D, Flatten
+from tensorflow.keras.optimizers import SGD, Adam, RMSprop
+from tensorflow.keras.metrics import Precision, Recall
+from tensorflow.keras.callbacks import ModelCheckpoint
 
 
-def cnn_model(parameters, x_train, y_train, x_test, y_test):
+def load_all_ConnMats(base_path):
+
+    dirs = os.listdir(base_path)
+    r = re.compile("sub-")
+    filtered = [folder for folder in dirs if r.match(folder)]
+
+    subs = [base_path + sub for sub in filtered]
+    subs = [sub + "/ConnMat" for sub in subs]
+
+    mat_paths = []
+    for sub in subs:
+        tmp_dir = os.listdir(sub)
+        tmp_path = [file for file in tmp_dir if ".mat" in file][0]
+        tmp_path = sub + "/" + tmp_path
+        mat_paths.append(tmp_path)
+
+    input_struct = np.zeros((len(mat_paths), 247, 247, 1))
+    for cnt, mat in enumerate(mat_paths):
+        tmp_mat = loadmat(mat)
+        tmp_mat = tmp_mat['myConnMat']
+        input_struct[cnt, :, :, 0] = tmp_mat
+
+    return input_struct
+
+
+def cnn_model(parameters, x_train, y_train):
     """
     param parameters:
     return: model, history
@@ -24,8 +52,10 @@ def cnn_model(parameters, x_train, y_train, x_test, y_test):
 
     # If SMOTETomek is wanted to be applied
     if parameters['imbalanced']:
-        smtom = SMOTETomek(random_state=42)
-        xtr, ytr = smtom.fit_resample(xtr, ytr)
+        arr = np.reshape(xtr, (xtr.shape[0], 247*247))
+        smtom = SMOTETomek(sampling_strategy='auto', random_state=42)
+        xtr_res, ytr = smtom.fit_resample(arr, ytr)
+        xtr = np.reshape(xtr_res, newshape=(xtr_res.shape[0], 247, 247, 1))
 
     # input_variables = xtr.shape[1]
 
@@ -38,6 +68,12 @@ def cnn_model(parameters, x_train, y_train, x_test, y_test):
     model.add(MaxPooling2D(pool_size=(2, 2)))
 
     model.add(Conv2D(neurons[1], kernel_size=(3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Conv2D(neurons[2], kernel_size=(3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Conv2D(neurons[3], kernel_size=(3, 3), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
 
     model.add(Flatten())
@@ -66,40 +102,64 @@ def cnn_model(parameters, x_train, y_train, x_test, y_test):
     return history, model
 
 
-# x_train = pd.read_csv('C:\\', encoding='latin1')
-# y_train = pd.read_csv('C:\\', encoding='latin1')
-# x_test = pd.read_csv('C:\\', encoding='latin1')
-# y_test = pd.read_csv('C:\\', encoding='latin1')
+base_path = "D:/PROCESSED_ADNI_CONTROL_GROUP/results/"
+input_struct_NC = load_all_ConnMats(base_path)
 
-mat = loadmat("C:/Users/imano/Desktop/MU/PBL/PBL-NEUROMOD/ConnMat_sub-ADNI011S4827_ses-M60.mat")
-mat = mat['myConnMat']
-x_train = np.zeros((6, 247, 247, 1))
-x_train[0, :, :, 0] = mat
-x_train[1, :, :, 0] = mat
-x_train[2, :, :, 0] = mat
-x_train[3, :, :, 0] = mat
-x_train[4, :, :, 0] = mat
-x_train[5, :, :, 0] = mat
-y_train = np.array([0, 0, 0, 0, 0, 0])
+base_path = "D:/PROCESSED_ADNI_AD_GROUP/PROCESSED_AD_GROUP/"
+input_struct_AD = load_all_ConnMats(base_path)
 
-x_test = np.zeros((2, 247, 247, 1))
-x_test[0, :, :, 0] = mat
-x_test[1, :, :, 0] = mat
-y_test = np.array([0, 0])
+x_train = np.concatenate((input_struct_AD, input_struct_NC), axis=0)
+y_train = np.zeros((input_struct_NC.shape[0]+input_struct_AD.shape[0]))
+y_train[input_struct_NC.shape[0]:input_struct_NC.shape[0]+input_struct_AD.shape[0]] = 1
 
-parameters = {'num_of_neurons': [32, 32],  # [layer0, layer1, layer2, ...]
+base_path = "D:/TEST/"
+x_test = load_all_ConnMats(base_path)
+
+y_test = np.array([0, 0, 0, 0, 0, 1, 1, 1, 1, 1])
+
+parameters = {'num_of_neurons': [128, 64, 32, 16],  # [layer0, layer1, layer2, ...]
               'dropout_vals': [0.5, 0.5, 0.5],
               'batch_size': 64,  # 32, 64, 128, ...
-              'num_of_layers': 2,  # total = num_of_layers + 1
-              'lr': 0.0001,
+              'num_of_layers': 4,  # total = num_of_layers + 1
+              'lr': 0.001,
               # 'sgd_momentum': 0.4371162594318422, # Just if SGD optimizer is selected
-              'epoch': 5000,
-              'imbalanced': False}
+              'epoch': 30,
+              'imbalanced': True}
 
-history, model = cnn_model(parameters, x_train, y_train, x_test, y_test)
+history, model = cnn_model(parameters, x_train, y_train)
 output_dir = './output_CNN/'
-model.save(output_dir + 'output_CNN_model.h5')
+# model.save(output_dir + 'output_CNN_model.h5')
 
-scores = pd.DataFrame()
-loss, accuracy, auc, pre, rec = model.evaluate(x_test, y_test, verbose=1)
+plt.figure()
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('Loss')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Test'], loc='best')
+plt.show()
+plt.savefig(output_dir + 'loss')
 
+plt.figure()
+plt.plot(history.history['auc'])
+plt.plot(history.history['val_auc'])
+plt.title('AUC')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Test'], loc='best')
+plt.show()
+
+loss, accuracy, auc, pre, rec = model.evaluate(x_test, y_test, verbose=0)
+
+f = open(output_dir + 'testing_results.txt', 'w')
+print(f"Loss: {loss}\n"
+      f"Accuracy: {accuracy}\n"
+      f"AUC: {auc}\n"
+      f"Precision: {pre}\n"
+      f"Recall: {rec}\n", file=f)
+
+print("\n------------------- EVALUATION RESULTS -------------------")
+print(f"Loss: {loss}\n"
+      f"Accuracy: {accuracy}\n"
+      f"AUC: {auc}\n"
+      f"Precision: {pre}\n"
+      f"Recall: {rec}\n")
+print("----------------------------------------------------------")
