76375425e1fb33a6c759e61c5017daadbb6c313d
iisasa <imanol.isasa@alumni.mondragon.edu>
2022-05-31 12:07:21 +0200

    CNN Training started and tracked

--- CNN_model_complete.py
+++ CNN_model_complete.py
@@ -1,6 +1,7 @@
 import os
 import re
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
@@ -15,31 +16,9 @@ from tensorflow.keras.metrics import Precision, Recall
 from tensorflow.keras.callbacks import ModelCheckpoint
 from tensorflow.keras.backend import clear_session
 import tensorflow as tf
+from datetime import datetime
 
-
-def load_all_ConnMats(base_path):
-
-    dirs = os.listdir(base_path)
-    r = re.compile("sub-")
-    filtered = [folder for folder in dirs if r.match(folder)]
-
-    subs = [base_path + sub for sub in filtered]
-    subs = [sub + "/ConnMat" for sub in subs]
-
-    mat_paths = []
-    for sub in subs:
-        tmp_dir = os.listdir(sub)
-        tmp_path = [file for file in tmp_dir if ".mat" in file][0]
-        tmp_path = sub + "/" + tmp_path
-        mat_paths.append(tmp_path)
-
-    input_struct = np.zeros((len(mat_paths), 247, 247, 1))
-    for cnt, mat in enumerate(mat_paths):
-        tmp_mat = loadmat(mat)
-        tmp_mat = tmp_mat['myConnMat']
-        input_struct[cnt, :, :, 0] = tmp_mat
-
-    return input_struct
+from PBL_DWI_lib.load_all_ConnMats import load_all_ConnMats
 
 
 def cnn_model(parameters, x_train, y_train):
@@ -56,15 +35,13 @@ def cnn_model(parameters, x_train, y_train):
 
     # If SMOTETomek is wanted to be applied
     if parameters['imbalanced']:
-        arr = np.reshape(xtr, (xtr.shape[0], 247*247))
+        arr = np.reshape(xtr, (xtr.shape[0], 247 * 247))
         smtom = SMOTETomek(sampling_strategy='auto', random_state=42)
         xtr_res, ytr = smtom.fit_resample(arr, ytr)
         xtr = np.reshape(xtr_res, newshape=(xtr_res.shape[0], 247, 247, 1))
 
-    # input_variables = xtr.shape[1]
-
     neurons = parameters['num_of_neurons']
-    dropouts = parameters['dropout_vals']
+    # dropouts = parameters['dropout_vals']
 
     # Build model architecture
     model = Sequential()
@@ -116,8 +93,8 @@ base_path = "D:/PROCESSED_ADNI_AD_GROUP/PROCESSED_AD_GROUP/"
 input_struct_AD = load_all_ConnMats(base_path)
 
 x_train = np.concatenate((input_struct_NC, input_struct_AD), axis=0)
-y_train = np.zeros((input_struct_NC.shape[0]+input_struct_AD.shape[0]))
-y_train[input_struct_NC.shape[0]:input_struct_NC.shape[0]+input_struct_AD.shape[0]] = 1
+y_train = np.zeros((input_struct_NC.shape[0] + input_struct_AD.shape[0]))
+y_train[input_struct_NC.shape[0]:input_struct_NC.shape[0] + input_struct_AD.shape[0]] = 1
 
 base_path = "D:/TEST/NC/"
 x_test_NC = load_all_ConnMats(base_path)
@@ -128,14 +105,14 @@ x_test_AD = load_all_ConnMats(base_path)
 x_test = np.concatenate((x_test_NC, x_test_AD), axis=0)
 y_test = np.array([0, 0, 0, 0, 0, 1, 1, 1, 1, 1])
 
-parameters = {'num_of_neurons': [128, 64, 32, 16],  # [layer0, layer1, layer2, ...]
+parameters = {'num_of_neurons': [64, 64, 64, 16],  # [layer0, layer1, layer2, ...]
               'dropout_vals': [0.5, 0.5, 0.5],
-              'batch_size': 32,  # 32, 64, 128, ...
+              'batch_size': 8,  # 32, 64, 128, ...
               'num_of_layers': 4,  # total = num_of_layers + 1
               'lr': 0.001,
               # 'sgd_momentum': 0.4371162594318422, # Just if SGD optimizer is selected
-              'epoch': 300,
-              'imbalanced': True}
+              'epoch': 30,
+              'imbalanced': False}
 
 history, model = cnn_model(parameters, x_train, y_train)
 output_dir = './output_CNN/'
@@ -165,6 +142,7 @@ cm = confusion_matrix(y_test, np.round(y_pred))
 plt.figure()
 categories = ['NC', 'AD']
 sns.heatmap(cm, annot=True, cmap='Blues')
+plt.savefig(output_dir + 'Conf_map')
 plt.show()
 
 f = open(output_dir + 'testing_results.txt', 'w')
@@ -181,6 +159,26 @@ print(f"Loss: {loss}\n"
       f"Precision: {pre}\n"
       f"Recall: {rec}\n")
 print("----------------------------------------------------------")
+
+csv_tmp = pd.DataFrame({'Datetime': [datetime.now().strftime("%m/%d/%Y, %H:%M:%S")],
+                        'num_of_neurons': [str(parameters['num_of_neurons'])],
+                        'batch_size': [parameters['batch_size']],
+                        'lr': [parameters['lr']],
+                        'epoch': [parameters['epoch']],
+                        'imbalanced': [parameters['imbalanced']],
+                        'Loss': [loss],
+                        'Acc': [accuracy],
+                        'AUC': [auc],
+                        'Pre': [pre],
+                        'Rec': [rec]})
+
+# trials=pd.DataFrame(columns=['Datetime', 'num_of_neurons', 'batch_size', 'lr', 'epoch', 'imbalanced', 'Loss', 'Acc', 'AUC', 'Pre', 'Rec'])
+# trials.to_csv("trials.csv", index=False)
+
+csv_path = "C:/Users/imano/Desktop/MU/PBL/PBL-NEUROMOD/output_CNN/trials.csv"
+csv = pd.read_csv(csv_path)
+csv = pd.concat([csv, csv_tmp])
+csv.to_csv(csv_path, index=False)
 
 clear_session()
 
---
+++ PBL_DWI_lib/load_all_ConnMats.py
@@ -0,0 +1,28 @@
+import os
+import re
+import numpy as np
+from scipy.io import loadmat
+
+
+def load_all_ConnMats(base_path):
+    dirs = os.listdir(base_path)
+    r = re.compile("sub-")
+    filtered = [folder for folder in dirs if r.match(folder)]
+
+    subs = [base_path + sub for sub in filtered]
+    subs = [sub + "/ConnMat" for sub in subs]
+
+    mat_paths = []
+    for sub in subs:
+        tmp_dir = os.listdir(sub)
+        tmp_path = [file for file in tmp_dir if ".mat" in file][0]
+        tmp_path = sub + "/" + tmp_path
+        mat_paths.append(tmp_path)
+
+    input_struct = np.zeros((len(mat_paths), 247, 247, 1))
+    for cnt, mat in enumerate(mat_paths):
+        tmp_mat = loadmat(mat)
+        tmp_mat = tmp_mat['myConnMat']
+        input_struct[cnt, :, :, 0] = tmp_mat
+
+    return input_struct
