07c594d1caad84cad34e3cfa6d8963b7e43991a0
iisasa <imanol.isasa@alumni.mondragon.edu>
2022-06-08 18:09:45 +0200

    bug fixed and LDA added

--- ML_models.py
+++ ML_models.py
@@ -6,133 +6,39 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix
 from sklearn.naive_bayes import GaussianNB
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+
+
+def save_print_metrics(rates, model_name):
+    f = open(OUTPUT_DIR + model_name + '.txt', 'w')
+
+    tp = rates['tp']
+    fp = rates['fp']
+    tn = rates['tn']
+    fn = rates['fn']
+
+    print("------------ "+ model_name + " L1O ------------")
+    print("------------ "+ model_name + " L1O ------------", file=f)
+    print(df)
+    print(df.to_string(), file=f)
+    print("------------ "+ model_name + " MEAN ------------")
+    print("------------ "+ model_name + " MEAN ------------", file=f)
+    print(f"Accuracy: {np.mean(df['Accuracy'])},\n"
+          f"Sensitivity: {tp / (tp + fn)},\n"
+          f"Specificity: {tn / (tn + fp)},\n"
+          f"Precision: {tp / (tp + fp)}")
+    print(f"Accuracy: {np.mean(df['Accuracy'])},\n"
+          f"Sensitivity: {tp / (tp + fn)},\n"
+          f"Specificity: {tn / (tn + fp)},\n"
+          f"Precision: {tp / (tp + fp)}", file=f)
+
 
 OUTPUT_DIR = "./output_ML/MD/"
+FILENAME = 'MDdual'
 tn = fp = tp = fn = 0
 
 path = "C:/Users/imano/Desktop/MU/PBL/PBL-NEUROMOD/Voxel_metrics/MD_dual.csv"
 df = pd.read_csv(path, encoding='latin1', delimiter=',')
-# df = df[["A8m",
-# "A8dl",
-# "A9l",
-# "A6dl",
-# "A6m",
-# "A9m",
-# "A10m",
-# "A9/46d",
-# "IFJ",
-# "A46",
-# "A9/46v",
-# "A8vl",
-# "A6vl",
-# "A10l",
-# "A44d",
-# "IFS",
-# "A45c",
-# "A45r",
-# "A44op",
-# "A44v",
-# "A14m",
-# "A12/47o",
-# "A11l",
-# "A11m",
-# "A13",
-# "A12/47l",
-# "A4hf",
-# "A6cdl",
-# "A4ul",
-# "A4tl",
-# "A6cvl",
-# "A1/2/3ll",
-# "A4ll",
-# "A38m",
-# "A41/42",
-# "TE1.0/TE1.2",
-# "A22c",
-# "A38l",
-# "A22r",
-# "A21c",
-# "A21r",
-# "A37dl",
-# "aSTS",
-# "A20iv",
-# "A37elv",
-# "A20r",
-# "A20il",
-# "A37vl",
-# "A20cl",
-# "A20cv",
-# "A20rv",
-# "A37mv",
-# "A37lv",
-# "A35/36r",
-# "A35/36c",
-# "TL",
-# "A28/34",
-# "TI",
-# "TH",
-# "rpSTS",
-# "cpSTS",
-# "A7r",
-# "A7c",
-# "A5l",
-# "A7pc",
-# "A7ip",
-# "A39c",
-# "A39rd",
-# "A40rd",
-# "A40c",
-# "A39rv",
-# "A40rv",
-# "A7m",
-# "A5m",
-# "dmPOS",
-# "A31",
-# "A1/2/3ulhf",
-# "A1/2/3tonIa",
-# "A2",
-# "G",
-# "vIa",
-# "dIa",
-# "vId/vIg",
-# "dIg",
-# "dId",
-# "A23d",
-# "A24rv",
-# "A32p",
-# "A23v",
-# "A24cd",
-# "A23c",
-# "A32sg",
-# "cLinG",
-# "rCunG",
-# "rLinG",
-# "vmPOS",
-# "mOccG",
-# "V5/MT+",
-# "iOccG",
-# "msOccG",
-# "lsOccG",
-# "mAmyg",
-# "lAmyg",
-# "rHipp",
-# "cHipp",
-# "vCa",
-# "GP",
-# "NAC",
-# "vmPu",
-# "dCa",
-# "dlPu",
-# "mPFtha",
-# "mPMtha",
-# "Stha",
-# "rTtha",
-# "PPtha",
-# "Otha",
-# "cTtha",
-# "lPFtha",
-# "Label"
-# ]]
 
 y_train_all = np.array(df['Label'])
 
@@ -168,21 +74,8 @@ for i in range(x_train_all.shape[0] - 1):
 
     df.loc[i, 'Accuracy'] = svc.score(x_test, y_test)
 
-f = open(OUTPUT_DIR + 'SVM_L1O_MDdual.txt', 'w')
-print("------------ SVM L1O ------------")
-print("------------ SVM L1O ------------", file=f)
-print(df)
-print(df.to_string(), file=f)
-print("------------ SVM MEAN ------------")
-print("------------ SVM MEAN ------------", file=f)
-print(f"Accuracy: {np.mean(df['Accuracy'])},\n"
-      f"Sensitivity: {tp / (tp + fn)},\n"
-      f"Specificity: {tn / (tn + fp)},\n"
-      f"Precision: {tp / (tp + fp)}")
-print(f"Accuracy: {np.mean(df['Accuracy'])},\n"
-      f"Sensitivity: {tp / (tp + fn)},\n"
-      f"Specificity: {tn / (tn + fp)},\n"
-      f"Precision: {tp / (tp + fp)}", file=f)
+true_false_rates = {'tp': tp, 'tn': tn, 'fn': fn, 'fp': fp}
+save_print_metrics(true_false_rates, 'SVM_L1O_' + FILENAME)
 
 """RF model"""
 tn = fp = tp = fn = 0
@@ -205,35 +98,17 @@ for i in range(x_train_all.shape[0] - 1):
 
     df.loc[i, 'Accuracy'] = rf.score(x_test, y_test)
 
-    if svc_pred == 0 and y_test == 0:
+    if rf_pred == 0 and y_test == 0:
         tn += 1
-    elif svc_pred == 1 and y_test == 1:
+    elif rf_pred == 1 and y_test == 1:
         tp += 1
-    elif svc_pred == 0 and y_test == 1:
+    elif rf_pred == 0 and y_test == 1:
         fn += 1
-    elif svc_pred == 1 and y_test == 0:
+    elif rf_pred == 1 and y_test == 0:
         fp += 1
 
-if (tp + fp) == 0:
-    prec = 0
-else:
-    prec = tp / (tp + fp)
-
-f = open(OUTPUT_DIR + 'RF_L1O_MDdual.txt', 'w')
-print("------------ RF L1O ------------")
-print("------------ RF L1O ------------", file=f)
-print(df)
-print(df.to_string(), file=f)
-print("------------ RF MEAN ------------")
-print("------------ RF MEAN ------------", file=f)
-print(f"Accuracy: {np.mean(df['Accuracy'])},\n"
-      f"Sensitivity: {tp / (tp + fn)},\n"
-      f"Specificity: {tn / (tn + fp)},\n"
-      f"Precision: {prec}")
-print(f"Accuracy: {np.mean(df['Accuracy'])},\n"
-      f"Sensitivity: {tp / (tp + fn)},\n"
-      f"Specificity: {tn / (tn + fp)},\n"
-      f"Precision: {prec}", file=f)
+true_false_rates = {'tp': tp, 'tn': tn, 'fn': fn, 'fp': fp}
+save_print_metrics(true_false_rates, 'RF_L1O_' + FILENAME)
 
 """Naive Bayes model"""
 tn = fp = tp = fn = 0
@@ -256,33 +131,47 @@ for i in range(x_train_all.shape[0] - 1):
 
     df.loc[i, 'Accuracy'] = nb.score(x_test, y_test)
 
-    if svc_pred == 0 and y_test == 0:
+    if nb_pred == 0 and y_test == 0:
         tn += 1
-    elif svc_pred == 1 and y_test == 1:
+    elif nb_pred == 1 and y_test == 1:
         tp += 1
-    elif svc_pred == 0 and y_test == 1:
+    elif nb_pred == 0 and y_test == 1:
         fn += 1
-    elif svc_pred == 1 and y_test == 0:
+    elif nb_pred == 1 and y_test == 0:
         fp += 1
 
-if (tp + fp) == 0:
-    prec = 0
-else:
-    prec = tp / (tp + fp)
+true_false_rates = {'tp': tp, 'tn': tn, 'fn': fn, 'fp': fp}
+save_print_metrics(true_false_rates, 'NB_L1O_' + FILENAME)
 
-f = open(OUTPUT_DIR + 'NB_L1O_MDdual.txt', 'w')
-print("------------ NB L1O ------------")
-print("------------ NB L1O ------------", file=f)
-print(df)
-print(df.to_string(), file=f)
-print("------------ NB MEAN ------------")
-print("------------ NB MEAN ------------", file=f)
-print(f"Accuracy: {np.mean(df['Accuracy'])},\n"
-      f"Sensitivity: {tp / (tp + fn)},\n"
-      f"Specificity: {tn / (tn + fp)},\n"
-      f"Precision: {prec}")
-print(f"Accuracy: {np.mean(df['Accuracy'])},\n"
-      f"Sensitivity: {tp / (tp + fn)},\n"
-      f"Specificity: {tn / (tn + fp)},\n"
-      f"Precision: {prec}", file=f)
+"""LDA model"""
+tn = fp = tp = fn = 0
+df = pd.DataFrame()
+for i in range(x_train_all.shape[0] - 1):
+    x_train = x_train_all
+    y_train = y_train_all
 
+    x_test = x_train[i, :]
+    x_test = np.expand_dims(x_test, axis=0)
+    x_train = np.delete(x_train, i, axis=0)
+
+    y_test = y_train[i]
+    y_test = np.array([y_test])
+    y_train = np.delete(y_train, i, axis=0)
+
+    lda = LinearDiscriminantAnalysis()
+    lda.fit(x_train, y_train)
+    lda_pred = lda.predict(x_test)
+
+    df.loc[i, 'Accuracy'] = lda.score(x_test, y_test)
+
+    if lda_pred == 0 and y_test == 0:
+        tn += 1
+    elif lda_pred == 1 and y_test == 1:
+        tp += 1
+    elif lda_pred == 0 and y_test == 1:
+        fn += 1
+    elif lda_pred == 1 and y_test == 0:
+        fp += 1
+
+true_false_rates = {'tp': tp, 'tn': tn, 'fn': fn, 'fp': fp}
+save_print_metrics(true_false_rates, 'LDA_L1O_' + FILENAME)
